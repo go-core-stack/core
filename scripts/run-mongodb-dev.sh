32cb93d1358d0ce8edaa07cd30f45067871d22a3
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-04-07 10:11:00 +0000

    Enable Change streams for the dev mongodb deployment
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- scripts/run-mongodb-dev.sh
+++ scripts/run-mongodb-dev.sh
@@ -2,4 +2,23 @@
 
 set -ex
 
-sudo docker run -d --network host --name mongodb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=password mongo:8.0.6
+SCRIPT_DIR=$(dirname $0)
+
+if test -e "$SCRIPT_DIR/mongod.conf"
+then
+    CONFIG_FLAG="--config /etc/mymongo/mongod.conf"
+    VOLUME_FLAG="-v $SCRIPT_DIR:/etc/mymongo"
+
+    if test -e "$SCRIPT_DIR/mongod_key"
+    then
+        echo "skipping key generation"
+    else
+        openssl rand -base64 756 > $SCRIPT_DIR/mongod_key
+        chmod 400 $SCRIPT_DIR/mongod_key
+    fi
+    sudo chown 999:999 $SCRIPT_DIR/mongod_key
+fi
+
+sudo docker run -d --network host --name mongodb ${VOLUME_FLAG} -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=password mongo:8.0.6 ${CONFIG_FLAG}
+sleep 10
+sudo docker exec -i mongodb mongosh -u root -p password --eval 'rs.initiate()'
