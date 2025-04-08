540c7371c50a5e50d7d51525b689d1101bbd66f9
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-04-08 18:51:04 +0000

    Enable more test cases for database
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- db/mongo_test.go
+++ db/mongo_test.go
@@ -9,24 +9,61 @@ import (
 )
 
 func Test_ClientConnection(t *testing.T) {
-	config := &MongoConfig{
-		Host:     "localhost",
-		Port:     "27017",
-		Username: "root",
-		Password: "password",
-	}
+	t.Run("Valid_Auth_Config", func(t *testing.T) {
+		config := &MongoConfig{
+			Host:     "localhost",
+			Port:     "27017",
+			Username: "root",
+			Password: "password",
+		}
 
-	client, err := NewMongoClient(config)
+		client, err := NewMongoClient(config)
 
-	if err != nil {
-		t.Errorf("failed to connect to mongo DB Error: %s", err)
-		return
-	}
+		if err != nil {
+			t.Errorf("failed to connect to mongo DB Error: %s", err)
+			return
+		}
 
-	err = client.HealthCheck(context.Background())
-	if err != nil {
-		t.Errorf("failed to perform Health check with DB Error: %s", err)
-	}
+		err = client.HealthCheck(context.Background())
+		if err != nil {
+			t.Errorf("failed to perform Health check with DB Error: %s", err)
+		}
 
-	_ = client.GetDataStore("test")
+		_ = client.GetDataStore("test")
+	})
+
+	t.Run("InValid_Port", func(t *testing.T) {
+		config := &MongoConfig{
+			Host:     "localhost",
+			Port:     "abc",
+			Username: "root",
+			Password: "badPassword",
+		}
+		_, err := NewMongoClient(config)
+
+		if err == nil {
+			t.Errorf("Connection succeeded while using invalid port number")
+			return
+		}
+	})
+
+	t.Run("InValid_Auth_Config", func(t *testing.T) {
+		config := &MongoConfig{
+			Host:     "localhost",
+			Port:     "27017",
+			Username: "root",
+			Password: "badPassword",
+		}
+		client, err := NewMongoClient(config)
+
+		if err != nil {
+			t.Errorf("failed to connect to mongo DB Error: %s", err)
+			return
+		}
+
+		err = client.HealthCheck(context.Background())
+		if err == nil {
+			t.Errorf("Health Check for mongo DB passed while using wrong password")
+		}
+	})
 }
