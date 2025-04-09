18f8e4aea78374d4367313a01e79f739a96accda
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-04-09 08:04:40 +0000

    Enable functionality to handle add and remove of entry from mongo
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- db/mongo.go
+++ db/mongo.go
@@ -11,16 +11,96 @@ import (
 	"net"
 	"strconv"
 
-	"github.com/Prabhjot-Sethi/core/errors"
-
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
+
+	"github.com/Prabhjot-Sethi/core/errors"
 )
+
+type mongoCollection struct {
+	StoreCollection
+	parent  *mongoStore // handler for the parent mongo DB object
+	colName string      // name of the collection this collection object is working with
+	col     *mongo.Collection
+}
+
+// inserts one entry with given key and data to the collection
+// returns errors if entry already exists or if there is a connection
+// error with the database server
+func (c *mongoCollection) InsertOne(ctx context.Context, key interface{}, data interface{}) error {
+	if data == nil {
+		return errors.Wrap(errors.InvalidArgument, "db Insert error: No data to store")
+	}
+	if key == nil {
+		return errors.Wrap(errors.InvalidArgument, "db Insert error: No Key specified to store")
+	}
+
+	// convert data to bson document for transacting with mongo db library
+	marshaledData, err := bson.Marshal(data)
+	if err != nil {
+		// return if any error occured
+		return err
+	}
+
+	bd := bson.D{}
+	err = bson.Unmarshal(marshaledData, &bd)
+	if err != nil {
+		// return if any error occured
+		return err
+	}
+
+	// key is already nil checked
+	// set the primary key to specified key.
+	//
+	// TODO(prabhjot) check if we want to allow nil key at some point
+	// Typically mongodb allows inserts with key not specified
+	// where it auto allocates the key to random id
+	primKey := bson.E{
+		Key:   "_id", // set primary key
+		Value: key,
+	}
+	bd = append(bd, primKey)
+
+	_, err = c.col.InsertOne(ctx, bd)
+	if err != nil {
+		// TODO(prabhjot) we may need to identify and differentiate
+		// Already Exist error here.
+		return err
+	}
+	return nil
+}
+
+// remove one entry from the collection matching the given key
+func (c *mongoCollection) DeleteOne(ctx context.Context, key interface{}) error {
+	resp, err := c.col.DeleteOne(ctx, bson.M{"_id": key})
+	if err != nil {
+		// TODO(prabhjot) we may need to identify and differentiate
+		// Not found error here
+		return err
+	}
+	if resp.DeletedCount == 0 {
+		return errors.Wrap(errors.NotFound, "No Document found")
+	}
+
+	return nil
+}
 
 type mongoStore struct {
 	Store
 	db *mongo.Database
+}
+
+func (s *mongoStore) GetCollection(name string) StoreCollection {
+	handle := s.db.Collection(name)
+	c := &mongoCollection{
+		parent:  s,
+		colName: name,
+		col:     handle,
+	}
+
+	return c
 }
 
 type mongoClient struct {
@@ -93,6 +173,13 @@ func (c *mongoClient) GetDataStore(dbName string) Store {
 	//go mongoStore.ReadRefSchema(ctx)
 
 	return mongoStore
+}
+
+// gets Mongo DB collection for given collection name
+// inside a database specified with db name
+func (c *mongoClient) GetCollection(dbName, col string) StoreCollection {
+	s := c.GetDataStore(dbName)
+	return s.GetCollection(col)
 }
 
 func (c *mongoClient) HealthCheck(ctx context.Context) error {
--- db/store.go
+++ db/store.go
@@ -10,12 +10,30 @@ import (
 	"context"
 )
 
-type Store interface {
+// interface definition for a collection in store
+type StoreCollection interface {
+	// insert one entry to the collection for the given key and data
+	InsertOne(ctx context.Context, key interface{}, data interface{}) error
+
+	// remove one entry from the collection matching the given key
+	DeleteOne(ctx context.Context, key interface{}) error
 }
 
+// interface definition for a store, responsible for holding group
+// of collections
+type Store interface {
+	// Gets collection corresponding to the collection name
+	GetCollection(col string) StoreCollection
+}
+
+// interface definition for Client corresponding to a store and
 type StoreClient interface {
 	// Get the Data Store interface given the client interface
 	GetDataStore(dbName string) Store
+
+	// Gets collection corresponding to the collection name inside
+	// the requested database name
+	GetCollection(dbName, col string) StoreCollection
 
 	// Health Check, if the Store is connectable and healthy
 	// returns the status of health of the server by means of
