baa6fdc7c9cc04924f89b529d4e3a8061e652a2c
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-04-17 20:22:36 +0000

    Remove unused file
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- reconciler/notifier.go
+++
@@ -1,6 +0,0 @@
-// Copyright © 2025 Prabhjot Singh Sethi, All Rights reserved
-// Author: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
-
-package reconciler
-
-type ReconcilerFunc func(k any) (*Result, error)
