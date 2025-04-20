8d7421decb61e842d91dc642e4e0a56455b20018
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-04-20 10:36:08 +0000

    Add Observers capability to Provider Table
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

---
+++ sync/observer.go
@@ -0,0 +1,76 @@
+// Copyright © 2025 Prabhjot Singh Sethi, All Rights reserved
+// Author: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
+
+package sync
+
+import (
+	"sync"
+
+	"github.com/Prabhjot-Sethi/core/reconciler"
+)
+
+type observerCountKey struct {
+	ExtKey any `bson:"_id.extKey,omitempty"`
+}
+
+type observerTable struct {
+	reconciler.ManagerImpl
+	mu        sync.RWMutex
+	providers map[any]struct{}
+}
+
+func (o *observerTable) getProviderList() []any {
+	list := []any{}
+	func() {
+		o.mu.Lock()
+		defer o.mu.Unlock()
+		for k := range o.providers {
+			list = append(list, k)
+		}
+	}()
+	return list
+}
+
+func (o *observerTable) isProviderAvailable(key any) bool {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	_, ok := o.providers[key]
+	return ok
+}
+
+func (o *observerTable) deleteProvider(key any) {
+	var ok bool
+	func() {
+		o.mu.Lock()
+		defer o.mu.Unlock()
+		_, ok = o.providers[key]
+		if ok {
+			delete(o.providers, key)
+		}
+	}()
+	if ok {
+		// since the observer is removed trigger an update
+		// for controllers
+		o.NotifyCallback(key)
+	}
+}
+
+func (o *observerTable) insertProvider(key any) {
+	var ok bool
+	func() {
+		o.mu.Lock()
+		defer o.mu.Unlock()
+		_, ok = o.providers[key]
+		if !ok {
+			o.providers[key] = struct{}{}
+		}
+	}()
+	if !ok {
+		// Notify an insert of new provider to providers
+		o.NotifyCallback(key)
+	}
+}
+
+func (o *observerTable) ReconcilerGetAllKeys() []any {
+	return o.getProviderList()
+}
--- sync/observer_test.go
+++ sync/observer_test.go
@@ -5,14 +5,35 @@ package sync
 
 import (
 	"context"
+	"log"
 	"testing"
 	"time"
 
 	"github.com/Prabhjot-Sethi/core/db"
 	"github.com/Prabhjot-Sethi/core/errors"
+	"github.com/Prabhjot-Sethi/core/reconciler"
 )
 
-func Test_ProviderBaseTesting(t *testing.T) {
+type MyObserver struct {
+	reconciler.Controller
+	providers map[string]struct{}
+	tbl       *ProviderTable
+}
+
+func (o *MyObserver) Reconcile(k any) (*reconciler.Result, error) {
+	key := k.(string)
+	if key == "" {
+		log.Panicln("Got invalid key response")
+	}
+	if o.tbl.IsProviderAvailable(key) {
+		o.providers[key] = struct{}{}
+	} else {
+		delete(o.providers, key)
+	}
+	return &reconciler.Result{}, nil
+}
+
+func Test_ObserverBaseTesting(t *testing.T) {
 	config := &db.MongoConfig{
 		Host:     "localhost",
 		Port:     "27017",
@@ -35,27 +56,48 @@ func Test_ProviderBaseTesting(t *testing.T) {
 	}
 
 	tbl, err := LocateProviderTable(s)
-
 	if err != nil {
 		t.Errorf("failed to locate provider Table: %s", err)
 	}
+
+	obs := &MyObserver{
+		providers: map[string]struct{}{},
+		tbl:       tbl,
+	}
+	tbl.Register("test-observer", obs)
 
 	provider, err := tbl.CreateProvider(context.Background(), "test-key")
 	if err != nil {
 		t.Errorf("failed to create Provider: %s", err)
 	}
+	time.Sleep(1 * time.Second)
+	if len(obs.providers) != 1 {
+		t.Errorf("Expected 1 provider but got %d", len(obs.providers))
+	}
+
 	providerDup, err := tbl.CreateProvider(context.Background(), "test-key")
 	if err != nil {
 		t.Errorf("failed to create Provider: %s", err)
+	}
+	time.Sleep(1 * time.Second)
+	if len(obs.providers) != 1 {
+		t.Errorf("Expected 1 provider but got %d", len(obs.providers))
 	}
 
 	provider1, err := tbl.CreateProvider(context.Background(), "test-key-1")
 	if err != nil {
 		t.Errorf("failed to create provider: %s", err)
 	}
+	time.Sleep(1 * time.Second)
+	if len(obs.providers) != 2 {
+		t.Errorf("Expected 2 provider but got %d", len(obs.providers))
+	}
 	_ = provider1.Close()
 	_ = providerDup.Close()
 	_ = provider.Close()
 
 	time.Sleep(2 * time.Second)
+	if len(obs.providers) != 0 {
+		t.Errorf("Expected 0 provider but got %d", len(obs.providers))
+	}
 }
